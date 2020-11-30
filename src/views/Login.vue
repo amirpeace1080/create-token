60b610e43f1aa925f72bdd2569ee42050205c084
amirpeace1080 <amirpeace1080@gmail.com>
2020-11-30 16:31:13 +0330

    first

---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input type="email" v-model="email" />
+    <br />
+    <input type="password" v-model="password" />
+    <br />
+    <br />
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: null,
+            password: null
+        }
+    },
+    methods: {
+        login(){
+            let email = this.email
+            let password = this.password
+            this.$store.dispatch('login', { email, password } )
+        }
+    },
+}
+</script>
+
+<style></style>
